3338f8fba7cd2796557868a4601a2ac530d61848
Shamistan Huseynov <shamistan.huseynov@abb-bank.az>
2021-12-02 21:12:13 +0400

    error in route table association is solved

--- 09-route-table.tf
+++ 09-route-table.tf
@@ -17,26 +17,26 @@ resource "aws_route_table" "back-private" {
 
 }
 
-resource "aws_route_table_association" "public" {
+resource "aws_route_table_association" "public1" {
 
   subnet_id = aws_subnet.front-public-phone-book-1.id
   route_table_id = aws_route_table.front-public.id
 }
 
-resource "aws_route_table_association" "public" {
+resource "aws_route_table_association" "public2" {
 
   subnet_id = aws_subnet.front-public-phone-book-2.id
   route_table_id = aws_route_table.front-public.id
 }
 
 
-resource "aws_route_table_association" "private" {
+resource "aws_route_table_association" "private1" {
 
   subnet_id = aws_subnet.back-private-phone-book-1.id
   route_table_id = aws_route_table.back-private.id
 }
 
-resource "aws_route_table_association" "private" {
+resource "aws_route_table_association" "private2" {
 
   subnet_id = aws_subnet.back-private-phone-book-2.id
   route_table_id = aws_route_table.back-private.id
