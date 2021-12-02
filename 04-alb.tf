0277c378b024ec787ab23d15a1ced2f0dd940845
Shamistan Huseynov <shamistan.huseynov@abb-bank.az>
2021-12-02 22:11:32 +0400

    working terraform

--- 04-alb.tf
+++ 04-alb.tf
@@ -8,10 +8,10 @@ resource "aws_lb" "pubic_alb" {
   ]
 
   subnets = [
-    aws_subnet.back-private-phone-book-1,
-    aws_subnet.back-private-phone-book-2,
-    aws_subnet.front-public-phone-book-1,
-    aws_subnet.front-public-phone-book-2
+    aws_subnet.back-private-phone-book-1.id,
+    aws_subnet.back-private-phone-book-2.id,
+    aws_subnet.front-public-phone-book-1.id,
+    aws_subnet.front-public-phone-book-2.id
   ]
 
   enable_deletion_protection = false
